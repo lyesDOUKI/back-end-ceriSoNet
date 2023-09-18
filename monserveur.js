b5283ba6aa52923a1587fddc7bcf1e6e0b342d8d
lyesdki <lyes.douki@alumni.univ-avignon.fr>
2023-09-18 19:59:59 +0200

    etape1 fini

--- monserveur.js
+++ monserveur.js
@@ -2,47 +2,23 @@ require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const PORT = process.env.PORT;
 const monserveur = express();
-const file = require("fs");
+const loginRoutes = require("./routes/login.js");
 const fs = require("fs");
 const https = require("https");
-const PATH_TO_HTML = process.env.PATH_TO_HTML;
+const bodyParser = require("body-parser");
 const options = {
     key: fs.readFileSync(process.env.KEY),
     cert: fs.readFileSync(process.env.CERTIFICATE),
 };
-
+////////////////////////////////////////////////////////////////////////////////
+monserveur.use(express.static(process.env.ROOT));
+monserveur.use(bodyParser.urlencoded({ extended : true}));
+monserveur.use(bodyParser.json());
 monserveur.all("/", (req, res) => {
     console.log("redirection vers /login pour le formulaire de connexion");
     res.redirect("/login");
 });
-
-monserveur.get("/login", (req, res) => {
-    
-    file.readFile(PATH_TO_HTML, "utf8", (err) => {
-        if (err)
-        {
-            res.status(500).send("erreur serveur lors de la lecture du fichier html");
-            return;
-        }
-        console.log("lecture index.html OK");
-        res.status(200).sendFile(PATH_TO_HTML);
-    });
-
-    if(req.query.username && req.query.password){
-        const username = req.query.username;
-        const password = req.query.password;
-        console.log("username : ", JSON.stringify(username));
-        console.log("password : ", JSON.stringify(password));
-    }
-});
-
-monserveur.get("/test", (req, res) => {
-    console.log("testing route /test");
-    res.status(200).send("test route/test OK!");
-});
-
-monserveur.use(express.static(process.env.ROOT));
-
+monserveur.use(loginRoutes);
 https.createServer(options, monserveur).listen(PORT, () => {
     console.log("Le serveur est lancé sur le port : ", PORT);
 });
---
+++ routes/login.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const loginRouter = express.Router();
+const PATH_TO_HTML = process.env.PATH_TO_HTML;
+const file = require("fs");
+
+loginRouter.get("/login", (req, res) => {
+    file.readFile(PATH_TO_HTML, "utf8", (err) => {
+        if (err)
+        {
+            res.status(500).send("erreur serveur lors de la lecture du fichier html");
+            return;
+        }
+        console.log("lecture index.html OK");
+        res.status(200).sendFile(PATH_TO_HTML);
+    });
+});
+
+loginRouter.post('/login', (req,res) => {
+
+    if(req.body.username && req.body.password){
+        const username = req.body.username;
+        const password = req.body.password;
+        console.log("username : ", username);
+        console.log("password : ", password);
+        res.status(200).send("login OK! bienvenue " + username);
+    }else{
+	    console.log("données non saisie");
+    }
+});
+
+module.exports = loginRouter;
